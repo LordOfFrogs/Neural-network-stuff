207cf05d2f7d7c6b89862a684587452b24bbe1ca
Noah Dell <ndtech260@gmail.com>
2021-03-08 20:16:47 -0500

    bitfinex

--- Neural network 2/Stock trader.py
+++ Neural network 2/Stock trader.py
@@ -1,61 +1,81 @@
 from time import time
+
+from bitfinex.bitfinex_v2 import api_v2
 from Net import Network, ActivationLayer, FCLayer
 from ActivationsLosses import Activations, Loss
 import numpy as np
-
+import bitfinex
 from datetime import datetime as dt, timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
 from ProgressBar import ProgressBar
 from pytz import timezone
+import time
+
+tz = timezone('EST')
+epoch = dt.fromtimestamp(0, tz=tz)
+
+
+def unix_time_millis(dt):
+    return time.mktime(dt.timetuple()) * 1000
+
+
+def fetch_data(start, stop, symbol, interval='1m', tick_limit=1000, step=60000000):
+    api = bitfinex.bitfinex_v2.api_v2()
+
+    data = []
+    start = start - step
+    while start < stop:
+        start += step
+        end = start + step
+        res = api.candles(symbol=symbol, interval=interval,
+                          limit=tick_limit, start=start, end=end)
+        data.extend(res)
+        print('Retrieving data from {} to {} for {}'.format(pd.to_datetime(
+            start, unit='ms'), pd.to_datetime(end, unit='ms'), symbol))
+        time.sleep(0)
+        
+    # Remove error messages
+    ind = [np.ndim(x) != 0 for x in data]
+    data = [i for (i, v) in zip(data, ind) if v]
+
+    # Create pandas data frame and clean data
+    names = ['time', 'open', 'close', 'high', 'low', 'volume']
+    df = pd.DataFrame(data, columns=names)
+    df.drop_duplicates(inplace=True)
+    df.set_index('time', inplace=True)
+    df.sort_index(inplace=True)
+
+    data = np.array(df['close'])
+    return data
+
 
 startingMoney = 5000.00
 money = startingMoney
-stocks = 0
-ticker = 'BTC-USD'
+shares = 0
+ticker = 'BTCUSD'
 risk = 5
-stocks_time = []
+n_inputs = 5000
 
-tz = timezone('EST')
-MINUTES_IN_DAY = 1415
-# get data
-historical_data = []
-mins = []
-maxes = []
-days = 29
-
-data = pd.read_csv("bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv")
-data.head()
-
-
-for i in range(days):
-    data = yf.download(ticker, start=dt.now(tz=tz) - timedelta(days=i+1), end=dt.now(tz=tz) - timedelta(days=i),
-                       interval='1m', progress=False).Close.to_numpy()
-    historical_data.append(data)
-    ProgressBar.printProgressBar(
-        i+1, days, 'Downloading Stock Data: ', length=50)
+raw_data = fetch_data(start=unix_time_millis(dt.now(tz=tz) - timedelta(days=10)),
+                       stop=unix_time_millis(dt.now(tz=tz)), symbol='btcusd')
 print('Download Complete')
-# prepare data
-for i in range(len(historical_data)):
-    mins.append(np.min(historical_data[i]))
-    historical_data[i] -= mins[i]
-    maxes.append(np.max(historical_data[i]))
-    historical_data[i] /= maxes[i]
 
-X_train = np.ndarray((len(historical_data), MINUTES_IN_DAY-1))
-y_train = np.ndarray((len(historical_data)))
-x = 0
-for i in historical_data:
-    X_train[x] = np.array(i[1:])
-    y_train[x] = np.array(i[0])
-    x += 1
-X_train = X_train.reshape((len(historical_data), 1, MINUTES_IN_DAY-1))
-y_train = y_train.reshape((len(historical_data), 1))
+X_train = []
+y_train = []
+for i in range(n_inputs - 1, raw_data.shape[0]):
+    X_train.append(raw_data[i-n_inputs+1:i-1])
+    y_train.append(raw_data[i])
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+y_train = np.reshape(y_train, (y_train.shape[0], 1))
 
 
 # create network
 net = Network(Loss.MSE, Loss.MSE_der)
-net.add(FCLayer(MINUTES_IN_DAY-1, 15))
+net.add(FCLayer(X_train.shape[0], 15))
 net.add(ActivationLayer(Activations.Sigmoid, Activations.Sigmoid_der))
 net.add(FCLayer(15, 15))
 net.add(ActivationLayer(Activations.Sigmoid, Activations.Sigmoid_der))
@@ -63,44 +83,11 @@ net.add(FCLayer(15, 1))
 net.add(ActivationLayer(Activations.Sigmoid, Activations.Sigmoid_der))
 net.fit(X_train, y_train, 0.1, 30)
 
-# region test
-'''# test
-test_data = yf.download(ticker, end=dt.today(), period='1wk',
-                        interval='1m', progress=False).Close.to_numpy()
-test_data = np.flip(test_data)
-min = np.min(test_data)
-test_data_norm = test_data - min
-max = np.max(test_data_norm)
-test_data_norm /= max
-for i in range(test_data_norm.shape[0] - 1 - MINUTES_IN_DAY):
-    X = test_data_norm[i:i+MINUTES_IN_DAY]
-    prediction = net.forward([[X]])
-    prediction = (prediction[0][0][0] * max) + min
-    if(prediction - test_data[i] > 0.0 and money > test_data[i]):
-        money -= test_data[i]*risk
-        stocks += risk
-    elif(prediction - test_data[i] < 0.0 and stocks > 0):
-        money += test_data[i]*risk
-        stocks -= risk
-    stocks_time.append(stocks)
-
-gain = money - startingMoney + stocks*test_data[-1]
-raw_gain = money - startingMoney
-print('Final Stock Price: %f' % (test_data[-1]))
-print(f'Money after week: {money}')
-print(f'Money gained after week: {gain}')
-print(f'Raw Gain: {raw_gain}')
-print(f'Stonks after week: {stocks}')
-plt.plot(stocks_time)
-plt.show()'''
-# endregion
-
 # run
-prevData = np.zeros((MINUTES_IN_DAY-1))
+prevData = np.zeros((n_inputs-1))
 while True:
-    data = yf.download(ticker, period='1d',
-                       end=dt.now(tz=tz), interval='1m', progress=False).Close
-    data = data.to_numpy()[:-1]
+    data = fetch_data(unix_time_millis(dt.now(tz=tz)-timedelta(days=1) +
+                                       timedelta(minutes=1)), unix_time_millis(dt.now(tz=tz)), ticker)
     if(prevData[0] != data[0]):
         min = np.min(data)
         X = data - min
